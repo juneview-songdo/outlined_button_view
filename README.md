802082056ff491b73be0ce0963381e05b98e9f01
june lee <melodysdreamj@gmail.com>
2024-04-15 10:10:16 +0900

    `

--- README.md
+++ README.md
@@ -15,9 +15,9 @@
     ```
 3. start the project by entering the following command.
     ```bash
-    flutter run [execute file path here] -d chrome
+    flutter run lib/app/_/_/interaction/view.blueprint/component/outlined_button_view/_/view.dart -d chrome
     ```
 
 ## Screenshots
-![]()
+![](https://github.com/juneview-songdo/outlined_button_view/assets/21379657/bd3fecda-7a53-4392-9719-de992153641e)
 
--- lib/app/_/_/interaction/view.blueprint/component/outlined_button_view/_/view.dart
+++ lib/app/_/_/interaction/view.blueprint/component/outlined_button_view/_/view.dart
@@ -26,13 +26,11 @@ class NewViewState extends State<NewView> with StateMother {
         side: BorderSide(
             color: Theme.of(context).textTheme.bodyMedium!.color!, width: 2.5),
       ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-        child: Text(widget.label,
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).textTheme.bodyMedium!.color)),
-      ),
+      child: Text(widget.label,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).textTheme.bodyMedium!.color))
+          .padding(vertical: 15, horizontal: 20),
     );
   }
 }
